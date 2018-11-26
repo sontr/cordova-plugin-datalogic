c868245d833cde7db258e2679052aca40696ba6a
Sachin Shankar <sachin.shankar@datalogic.com>
2018-11-26 11:05:53 -0800

    Added scanning feature

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -5,7 +5,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import com.datalogic.decode.PropertyID;
 
+import com.datalogic.decode.BarcodeManager;
+import com.datalogic.decode.DecodeException;
+import com.datalogic.decode.DecodeResult;
+import com.datalogic.decode.ReadListener;
+import com.datalogic.device.ErrorManager;
+
+import android.util.Log;
+
+
 public class Hello extends CordovaPlugin {
+
+    private final String LOGTAG = getClass().getName();
+    BarcodeManager decoder = null;
+    ReadListener listener = null;
+    static String result = null;
+    boolean multiTasking=true;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView){
+        super.initialize(cordova, webView);
+        //initializeDecoder();
+    }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -14,8 +35,14 @@ public class Hello extends CordovaPlugin {
 
             String name = data.getString(0);
             //String message = "Hello, " + name;
-            String message = Integer.toString(PropertyID.AIM_ENABLE);
-	    callbackContext.success(message);
+            String message = Integer.toString(PropertyID.AIM_ENABLE) + " " + LOGTAG;
+            if(result == null){
+                callbackContext.success(message);    
+            }
+            else{
+                callbackContext.success(result);
+            }
+	        
 
             return true;
 
@@ -23,6 +50,92 @@ public class Hello extends CordovaPlugin {
             
             return false;
 
+        }
+    }
+
+    @Override
+    public void onResume(boolean multiTasking){
+        super.onResume(multiTasking);
+        Log.i(LOGTAG, "onResume");
+
+        // If the decoder instance is null, create it.
+        if (decoder == null) { // Remember an onPause call will set it to null.
+            decoder = new BarcodeManager();
+        }
+
+        // From here on, we want to be notified with exceptions in case of errors.
+        ErrorManager.enableExceptions(true);
+
+        try {
+
+            // Create an anonymous class.
+            listener = new ReadListener() {
+
+                // Implement the callback method.
+                @Override
+                public void onRead(DecodeResult decodeResult) {
+                    // Change the result to the current received result.
+                    result = decodeResult.getText();
+                }
+
+            };
+
+            // Remember to add it, as a listener.
+            decoder.addReadListener(listener);
+
+        } catch (DecodeException e) {
+            Log.e(LOGTAG, "Error while trying to bind a listener to BarcodeManager", e);
+        }
+    }
+
+    @Override
+    public void onPause(boolean multiTasking) {
+        super.onPause(multiTasking);
+
+        Log.i(LOGTAG, "onPause");
+
+        // If we have an instance of BarcodeManager.
+        if (decoder != null) {
+            try {
+                // Unregister our listener from it and free resources.
+                decoder.removeReadListener(listener);
+
+                // Let the garbage collector take care of our reference.
+                decoder = null;
+            } catch (Exception e) {
+                Log.e(LOGTAG, "Error while trying to remove a listener from BarcodeManager", e);
+            }
+        }
+    }
+
+    private void initializeDecoder(){
+        // If the decoder instance is null, create it.
+        if (decoder == null) { // Remember an onPause call will set it to null.
+            decoder = new BarcodeManager();
+        }
+
+        // From here on, we want to be notified with exceptions in case of errors.
+        ErrorManager.enableExceptions(true);
+
+        try {
+
+            // Create an anonymous class.
+            listener = new ReadListener() {
+
+                // Implement the callback method.
+                @Override
+                public void onRead(DecodeResult decodeResult) {
+                    // Change the displayed text to the current received result.
+                    result = decodeResult.getText();
+                }
+
+            };
+
+            // Remember to add it, as a listener.
+            decoder.addReadListener(listener);
+
+        } catch (DecodeException e) {
+            Log.e(LOGTAG, "Error while trying to bind a listener to BarcodeManager", e);
         }
     }
 }
