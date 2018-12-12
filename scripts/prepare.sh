91e74ecaf57455708de1dea2be17f8512f366bb0
Cody Brookshear <cody.brookshear@datalogic.com>
2018-12-12 15:57:05 -0800

    commit config.xml update to git

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,4 +6,5 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd ..; pwd )"
 pushd $ROOT
 VERSION=$(node -e "console.log(require('./package.json').version)")
 node ./scripts/update-plugin-xml.js $VERSION
+git commit -a -m "automatically bump plugin to v$VERSION"
 popd
